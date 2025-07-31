957dd358373e7d7cb2db91ba11490f24ba724ea1
kadenrose <156358267+kadenrose@users.noreply.github.com>
2025-07-30 17:12:25 -0700

    started writing a form validation function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// test
+// JS written by Kaden Rose & 
+
+"use strict";
+
+// July 30th, starting a basic form validation function
+
+function formValidation1() {
+    // create a boolean variable that tells us if the form is true or not
+    let isValid = true;
+
+    // variables for the inputs
+    let inputName = document.getElementById(""); // name input
+    let inputMail = document.getElementById(""); // email input
+    let inputTel = document.getElementById(""); // phone input
+    let radioMail = document.getElementById(""); // preferred method of contact radio email
+    let radioTel = document.getElementById(""); // preferred method of contact radio phone
+}
